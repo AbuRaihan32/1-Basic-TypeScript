7875be78158dcec259e4ea6fd23d90cfb134802a
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-11-02 03:40:30 +0600

    rest operator and spread operator

---
+++ module1/src/1.7.ts
@@ -0,0 +1,45 @@
+{
+    // Spread operator
+    // rest operator
+
+
+
+    // spread operator ---> ja akta array er prottekta element k alada kore dey.
+
+    const friends: string[] = ['a', 'b', 'c'];
+    const friends1: string[] = ['e', 'd', 'sa'];
+
+    friends.push(...friends1)
+
+    
+
+    // spread in object 
+
+    const teachers = {
+        bengali : 'mahfuz',
+        english : 'johir',
+        math : 'alamin'
+    }
+
+    const teachers2 = {
+        b : 'b',
+        c : 'c',
+        d : 'd'
+    }
+
+    const teachersList = {
+        ...teachers,
+        ...teachers2
+    }
+
+
+    // rest operator
+
+
+    const messageFriends = (...friends : string[]) =>{
+        friends.forEach((elem : string) =>  console.log(`hi ${elem}`));
+    }
+
+    messageFriends('mahfuz' , 'shamim')
+
+}
