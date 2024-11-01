fd6a04d3f54d5ca6b9c9cf4a061d907cc8590eeb
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-11-01 23:38:32 +0600

    object

---
+++ module1/src/1.4.ts
@@ -0,0 +1,46 @@
+/** TypeScript Types: primitive 
+ *  1. string
+ *  2. number
+ *  3. boolean
+ *  4. null
+ *  5. undefined
+ *  6. symbol
+ *  */
+
+/** TypeScript Types: non-primitive 
+ *  1. array
+ *  2. object
+ *  3. tuple
+ *  */
+
+
+// string
+let a : string = 'Abu Raihan Mahfuz';
+
+// number
+let b : number = 25;
+
+// boolean
+let c : boolean = true;
+
+// null
+let d : null = null;
+
+// undefined
+let e : undefined = undefined;
+
+
+
+
+// array 
+let friends: string[] = ['younus', 'moien'];
+friends.push('shamim');
+
+let numbers: number[] = [10,20];
+
+
+// tuple --> array --> order --> values
+
+let tuple : [number, string] = [10, 'mahfuz'];
+
+let user : [string, number, boolean] = ['abu', 25, true]
---
+++ module1/src/1.5.ts
@@ -0,0 +1,54 @@
+
+// Reference type --> Object
+
+// siple Object
+let userObj_1 : {
+    firstName : string;
+    MiddleName : string;
+    LastName : string;
+} = {
+    firstName : 'Abu',
+    MiddleName : 'Raihan',
+    LastName : 'Mahfuz',
+}
+
+
+// example for Optional type 
+let userObj_2 : {
+    firstName : string;
+    MiddleName?: string;  // optional type
+    LastName : string;
+} = {
+    firstName : 'Abu',
+    LastName : 'Mahfuz',
+}
+
+
+// example for Literal Type
+let userObj_3 : {
+    company : 'it bari';  // literal type --> when the value define as type;
+    firstName : string;
+    MiddleName : string;
+    LastName : string;
+} = {
+    company: 'it bari',  
+    firstName : 'Abu',
+    MiddleName : 'Raihan',
+    LastName : 'Mahfuz',
+}
+
+
+// access modiy
+let userObj_4 : {
+    readonly company : 'it bari';  // literal type --> when the value define as type;
+    firstName : string;
+    MiddleName : string;
+    LastName : string;
+} = {
+    company: 'it bari',  
+    firstName : 'Abu',
+    MiddleName : 'Raihan',
+    LastName : 'Mahfuz',
+}
+
+userObj_4.company = 'PH';
