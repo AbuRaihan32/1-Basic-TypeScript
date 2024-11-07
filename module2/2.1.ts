7be0bf1d9bee20df2410056833b21fec138d9085
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-11-07 20:29:19 +0600

    type assertion

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -1,9 +1,32 @@
 {
  //
  
-    // type assertion
+    // type assertion ---> Atar mane hocche ami bishesh kono variable er type ki sheta as diye nirdharon kore deya.
 
 
+    let anything : any;
+
+    anything = 'hello world';
+
+    (anything as string);
+
+
+
+    const gmToKg = (value : string | number ) : number | string | undefined =>{
+      if(typeof value === 'string'){
+         const convertedValue = parseFloat(value) / 1000;
+         return `total kg is ${convertedValue}`
+      }
+      if(typeof value === 'number'){
+         return value /1000;
+      }
+    }
+
+    const numValue = gmToKg(1000) as number;
+    const strValue = gmToKg('20000') as string;
+
+
+    console.log(numValue, strValue)
 
 
 
