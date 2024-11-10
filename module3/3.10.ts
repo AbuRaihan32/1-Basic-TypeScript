03b67035e903dea44b9ba612c900ea50287a5c8c
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-11-10 23:07:52 +0600

    encapsulation

---
+++ module3/3.10.ts
@@ -0,0 +1,18 @@
+{
+    // Encapsulation in OOP   -----> access modifier
+
+
+
+    // core four topic in OOP 
+
+    /**
+     * 1. inheritance
+     * 2. polymorphism
+     * 3. abstraction
+     * 4. Encapsulation
+     */
+
+
+
+
+}
